8bd92993bf961d220fb6451d36b276c0a81e3ec0
Carlostrdom <carlos.a.t.m93@gmail.com>
2024-09-06 01:04:42 -0500

    casi listo

--- script/main.js
+++ script/main.js
@@ -1,50 +1,5 @@
-// let urlCharacters = "https://rickandmortyapi.com/api/character";
-
-// const { createApp } = Vue;
-
-// const app = createApp({
-//     data() {
-//         return {
-//             characters: [],
-//             textSearch: "",
-//             statusFilter: [],
-//             nextPage: null,
-//             prevPage: null,
-//         };
-//     },
-
-//     created() {
-//         this.fetchRickAndMortyData(urlCharacters);
-//     },
-
-//     methods: {
-//         fetchRickAndMortyData(url) {
-//             fetch(url)
-//                 .then(response => response.json())
-//                 .then(data => {
-//                     this.characters = data.results;
-//                     this.nextPage = data.info.next;
-//                     this.prevPage = data.info.prev;
-//                 });
-//         }
-//     },
-
-//     computed: {
-//         filteredCharacters() {
-//             return this.characters
-//                 .filter(character => {
-//                     // Filtra por nombre y estado
-//                     const matchesSearch = character.name
-//                         .toLowerCase()
-//                         .includes(this.textSearch.toLowerCase());
-//                     const matchesStatus = this.statusFilter.length === 0 ||
-//                         this.statusFilter.includes(character.status);
-//                     return matchesSearch && matchesStatus;
-//                 });
-//         }
-//     }
-// }).mount('#appHome');
 const urlCharacters = "https://rickandmortyapi.com/api/character";
+const urlEpisodes = "https://rickandmortyapi.com/api/episode";
 
 const { createApp } = Vue;
 
@@ -52,113 +7,115 @@ const app = createApp({
     data() {
         return {
             characters: [],
+            episodes: [],
             textSearch: "",
             statusFilter: [],
             speciesFilter: [],
             allSpecies: [],
-            nextPage: null,
-            prevPage: null,
             currentPage: 1,
             totalPages: 0,
+            nextPage: null,
+            prevPage: null,
+            isCharacters: true // Controla si estamos viendo personajes o episodios
         };
     },
 
     created() {
-        this.fetchRickAndMortyData(urlCharacters);
+        this.fetchData(urlCharacters);
     },
 
     methods: {
-        fetchRickAndMortyData(url) {
+        fetchData(url) {
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-                    this.characters = data.results;
-                    this.nextPage = data.info.next;
-                    this.prevPage = data.info.prev;
+                    if (this.isCharacters) {
+                        this.characters = data.results;
+                        this.nextPage = data.info.next;
+                        this.prevPage = data.info.prev;
 
-                    // Si nextPage es null, estamos en la última página, por lo que usamos prevPage para deducir la actual
-                    if (this.nextPage) {
-                        const urlParams = new URLSearchParams(this.nextPage.split('?')[1]);
-                        this.currentPage = parseInt(urlParams.get('page')) - 1;
-                    } else if (this.prevPage) {
-                        const urlParams = new URLSearchParams(this.prevPage.split('?')[1]);
-                        this.currentPage = parseInt(urlParams.get('page')) + 1;
+                        // Determinar la página actual
+                        const urlParams = new URLSearchParams(new URL(url).search);
+                        this.currentPage = parseInt(urlParams.get('page')) || 1;
+
+                        this.totalPages = Math.ceil(data.info.count / 20); // Ajuste según la cantidad total de personajes
+
+                        // Obtener todas las especies únicas
+                        const speciesSet = new Set(this.characters.map(character => character.species));
+                        this.allSpecies = Array.from(speciesSet);
                     } else {
-                        this.currentPage = 1; // Si no hay prev ni next, es la primera página
+                        this.episodes = data.results;
+                        this.nextPage = data.info.next;
+                        this.prevPage = data.info.prev;
+
+                        // Determinar la página actual
+                        const urlParams = new URLSearchParams(new URL(url).search);
+                        this.currentPage = parseInt(urlParams.get('page')) || 1;
+
+                        this.totalPages = Math.ceil(data.info.count / 20); // Ajuste según la cantidad total de episodios
                     }
-
-                    this.totalPages = Math.ceil(data.info.count / 20); // Ajuste según la cantidad total de personajes
-
-                    // Obtener todas las especies únicas
-                    const speciesSet = new Set(this.characters.map(character => character.species));
-                    this.allSpecies = Array.from(speciesSet);
                 });
+        },
+
+        goToPage(page) {
+            // Verificar que la página esté dentro del rango válido
+            if (page >= 1 && page <= this.totalPages) {
+                const url = this.isCharacters ? `https://rickandmortyapi.com/api/character?page=${page}` : `https://rickandmortyapi.com/api/episode?page=${page}`;
+                this.fetchData(url);
+            }
+        },
+
+        toggleData() {
+            // Alternar entre personajes y episodios
+            this.isCharacters = !this.isCharacters;
+            const url = this.isCharacters ? urlCharacters : urlEpisodes;
+            this.fetchData(url);
+        },
+
+        getEpisodeImage(episodeId) {
+            // Encontrar el episodio correspondiente
+            const episode = this.episodes.find(ep => ep.id === episodeId);
+
+            if (!episode) {
+                return 'https://via.placeholder.com/300x200?text=Episode+' + episodeId;
+            }
+
+            // Extraer los IDs de los personajes de la URL del episodio
+            const characterUrls = episode.characters;
+            if (characterUrls.length > 0) {
+                // Suponiendo que el primer personaje en la lista es suficiente
+                const characterUrl = characterUrls[0];
+                const characterId = characterUrl.split('/').pop(); // Extrae el ID de la URL
+
+                // Buscar el personaje por ID
+                const character = this.characters.find(char => char.id == characterId);
+                return character ? character.image : 'https://via.placeholder.com/300x200?text=Episode+' + episodeId;
+            }
+
+            return 'https://via.placeholder.com/300x200?text=Episode+' + episodeId;
         }
     }
     ,
-
     computed: {
-        filteredCharacters() {
-            return this.characters.filter(character => {
+        filteredData() {
+            const data = this.isCharacters ? this.characters : this.episodes;
+            return data.filter(item => {
                 // Filtra por nombre
-                const matchesSearch = character.name.toLowerCase().includes(this.textSearch.toLowerCase());
+                const nameMatch = item.name.toLowerCase().includes(this.textSearch.toLowerCase());
 
-                // Filtra por estado
-                const matchesStatus = this.statusFilter.length === 0 || this.statusFilter.includes(character.status);
+                if (this.isCharacters) {
+                    // Filtra por estado
+                    const statusMatch = this.statusFilter.length === 0 || this.statusFilter.includes(item.status);
 
-                // Filtra por especie
-                const matchesSpecies = this.speciesFilter.length === 0 || this.speciesFilter.includes(character.species);
+                    // Filtra por especies
+                    const speciesMatch = this.speciesFilter.length === 0 || this.speciesFilter.includes(item.species);
 
-                return matchesSearch && matchesStatus && matchesSpecies;
+                    return nameMatch && statusMatch && speciesMatch;
+                } else {
+                    return nameMatch;
+                }
             });
         }
     }
+
 }).mount('#appHome');
-
-// para obtener todas las especies únicas
-//
-
-
-// async function fetchAllSpecies() {
-//     let allSpecies = new Set();
-//     let url = baseUrl;
-
-//     while (url) {
-//         try {
-//             const response = await fetch(url);
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok');
-//             }
-
-//             const data = await response.json();
-//             data.results.forEach(character => {
-//                 allSpecies.add(character.species);
-//             });
-
-//             url = data.info.next;
-//         } catch (error) {
-//             console.error('Error al hacer fetch:', error);
-//             url = null; // Salir del bucle si hay un error
-//         }
-//     }
-
-//     return Array.from(allSpecies);
-// }
-
-// async function displaySpecies() {
-//     try {
-//         const species = await fetchAllSpecies();
-//         console.log('Todas las especies:', species);
-
-//         const speciesList = document.getElementById('species-list');
-//         species.forEach(species => {
-//             const speciesItem = document.createElement('div');
-//             speciesItem.textContent = species || 'Desconocida'; // Mostrar 'Desconocida' si la especie es una cadena vacía
-//             speciesList.appendChild(speciesItem);
-//         });
-//     } catch (error) {
-//         console.error('Error al obtener las especies:', error);
-//     }
-// }
-
-// displaySpecies();
